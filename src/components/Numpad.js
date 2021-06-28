96e429bd956061c5c3479e723185ea65ce62aaa0
Brian Mak <eggrollbot@gmail.com>
2021-06-28 00:05:14 -0700

    Centered numpad's buttons and increased font size

--- src/components/Numpad.js
+++ src/components/Numpad.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 
 function Numpad( props ) {
 	return (
-		<Grid container style={ { height: props.height, width: props.width } }>
+		<Grid container justify="center" style={ { height: props.height, width: props.width } }>
 			{
 				[ 7, 8, 9, 4, 5, 6, 1, 2, 3, 0 ].map( ( item ) =>
 					(
 						<Grid item key={ item } xs={ 4 }>
-							<Button color="primary" style={ { height: "100%", width: "100%" } }>
+							<Button color="primary" style={ { fontSize: "32px", height: "100%", width: "100%" } }>
 								{ item }
 							</Button>
 						</Grid>
