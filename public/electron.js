55fe797d9919aff7a69639f7324f9a5ea30ed6be
Brian Mak <eggrollbot@gmail.com>
2021-07-03 18:50:58 -0700

    Increased size of window

--- public/electron.js
+++ public/electron.js
@@ -5,8 +5,8 @@ const path = require( "path" );
 function createWindow( ) {
 	const mainWindow = new BrowserWindow( {
 		title: "Programmer's Calculator",
-		width: isDev ? 1050 : 750,
-		height: 600,
+		width: isDev ? 1100 : 800,
+		height: 650,
 		resizable: false,
 		webPreferences: {
 			preload: path.join( __dirname, "preload.js" )
