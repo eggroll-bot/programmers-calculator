624dda88edfabd77c408d31a0022596ce3ca9442
Brian Mak <eggrollbot@gmail.com>
2021-07-07 20:56:29 -0700

    Implemented SLL, SRL32, SRA, ROL32, and ROR32

--- src/util/advanced-operation-pad.js
+++ src/util/advanced-operation-pad.js
@@ -6,35 +6,82 @@ const advancedOperationPad = new Map( [
 		"SLL",
 		{
 			unary: false,
-			onClick: displayOperation.appendOperation
+			onClick: displayOperation.appendOperation,
+			evaluate: ( left, right ) => {
+				if ( right < 0n ) {
+					return undefined;
+				}
+
+				return left << right;
+			}
 		}
 	],
 	[
-		"SRL",
+		"SRL32",
 		{
 			unary: false,
-			onClick: displayOperation.appendOperation
+			onClick: displayOperation.appendOperation,
+			evaluate: ( left, right ) => {
+				if ( right < 0n ) {
+					return undefined;
+				}
+
+				const rightThirtyTwoBits = left & 4294967295n; // 2 ^ 32 - 1. Convert to unsigned 32-bit int.
+
+				return rightThirtyTwoBits >> right;
+			}
 		}
 	],
 	[
 		"SRA",
 		{
 			unary: false,
-			onClick: displayOperation.appendOperation
+			onClick: displayOperation.appendOperation,
+			evaluate: ( left, right ) => {
+				if ( right < 0n ) {
+					return undefined;
+				}
+
+				return left >> right;
+			}
 		}
 	],
 	[
-		"ROL",
+		"ROL32",
 		{
 			unary: false,
-			onClick: displayOperation.appendOperation
+			onClick: displayOperation.appendOperation,
+			evaluate: ( left, right ) => {
+				if ( right < 0n ) {
+					return undefined;
+				}
+
+				let rightThirtyTwoBits = left & 4294967295n; // 2 ^ 32 - 1. Convert to unsigned 32-bit int.
+
+				const rotateAmount = right % 32n;
+				const rotateResult = ( ( rightThirtyTwoBits << rotateAmount ) & 4294967295n ) | ( rightThirtyTwoBits >> ( 32n - rotateAmount ) );
+
+				return rotateResult >= 2147486648n ? rotateResult - 4294967296n : rotateResult;
+			}
 		}
 	],
 	[
-		"ROR",
+		"ROR32",
 		{
 			unary: false,
-			onClick: displayOperation.appendOperation
+			onClick: displayOperation.appendOperation,
+			evaluate: ( left, right ) => {
+				if ( right < 0n ) {
+					return undefined;
+				}
+
+				let rightThirtyTwoBits = left & 4294967295n; // 2 ^ 32 - 1. Convert to unsigned 32-bit int.
+
+				const rotateAmount = right % 32n;
+				const rotateResult = ( rightThirtyTwoBits >> rotateAmount ) | ( ( rightThirtyTwoBits << ( 32n - rotateAmount ) ) & 4294967295n );
+
+				return rotateResult >= 2147486648n ? rotateResult - 4294967296n : rotateResult;
+			}
 		}
 	],
 	[
