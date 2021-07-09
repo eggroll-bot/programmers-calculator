2995dfb492c2adba9e5037f72aeef234bbe0d2ad
Brian Mak <eggrollbot@gmail.com>
2021-07-08 20:59:16 -0700

    Removed extraneous comment and added preset-react to .babelrc

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,5 @@
 const { override, useBabelRc } = require( "customize-cra" );
 
 module.exports = override(
-	//removeInternalBabelPlugin( "@babel/plugin-transform-exponentiation-operator" )
 	useBabelRc( )
 );
